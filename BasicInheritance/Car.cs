92378c9de6adf71885c3e967f0be918483fbd04e
ash <59516869+xlaw-ash@users.noreply.github.com>
2020-02-12 12:54:22 +0530

    Add Basic Inheritance example to the course

---
+++ BasicInheritance/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicInheritance
+{
+    class Car
+    {
+        // Members
+        public readonly int maxSpeed;
+        private int currSpeed;
+
+        // Properties
+        public int Speed
+        {
+            get { return currSpeed; }
+            set
+            {
+                currSpeed = value;
+                if(currSpeed > maxSpeed)
+                {
+                    currSpeed = maxSpeed;
+                }
+            }
+        }
+
+        //Constructors
+        public Car(int max)
+        {
+            maxSpeed = max;
+        }
+    }
+}
---
+++ BasicInheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BasicInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine
+            (
+                "-----------------\n" +
+                "Basic Inheritance\n" +
+                "-----------------\n"
+            );
+
+            // Make a Car object and set Max Speed
+            Car myCar = new Car(80);
+
+            // Set the current speed and print it
+            myCar.Speed = 60;
+            Console.WriteLine($"Car Speed: {myCar.Speed} MPH");
+
+            myCar.Speed = 90;
+            Console.WriteLine($"Car Speed: {myCar.Speed} MPH");
+
+            Console.WriteLine
+            (
+                "\n--------------" +
+                "\nEnd of Program" +
+                "\n--------------"
+            );
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
