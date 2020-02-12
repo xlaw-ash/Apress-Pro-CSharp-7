83c035ffe194230e717dd01c89e1c2157b8aa4af
ash <59516869+xlaw-ash@users.noreply.github.com>
2020-02-12 12:03:54 +0530

    Add Car class and use it in Program

---
+++ SimpleClassExample/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleClassExample
+{
+    class Car
+    {
+        //The 'state' of the Car
+        public string petName;
+        public int currSpeed;
+
+        // The Functionality of the Car
+        // Using the expression-bodied member syntax introduced in C# 6
+        public void PrintState() => Console.WriteLine($"{petName} is going {currSpeed} MPH");
+        public void SpeedUp(int delta) => currSpeed += delta;
+    }
+}
--- SimpleClassExample/Program.cs
+++ SimpleClassExample/Program.cs
@@ -6,7 +6,33 @@ namespace SimpleClassExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine
+            (
+                "--------------------" +
+                "\nFun With Class Types\n" +
+                "--------------------\n"
+            );
+            
+            // Allocate and Configure a Car object
+            Car myCar = new Car();
+            myCar.petName = "Henry";
+            myCar.currSpeed = 10;
+
+            // Speed up the car a few times and print out the new state
+            for(int i = 0; i <= 10; i++)
+            {
+                myCar.SpeedUp(5);
+                myCar.PrintState();
+            }
+
+            Console.WriteLine
+            (
+                "\n--------------" +
+                "\nEnd of Program\n" +
+                "--------------"
+            );
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
         }
     }
 }
