d56dda4d244b761a89d36b5a5b128e5042b15ffe
ash <59516869+xlaw-ash@users.noreply.github.com>
2020-02-12 12:06:04 +0530

    Add EmployeeApp to the course

---
+++ EmployeeApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
