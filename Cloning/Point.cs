70dd3fb444ac4b63b3da8a922316d278ab9b0044
ash <59516869+xlaw-ash@users.noreply.github.com>
2020-02-13 13:55:30 +0530

    Add Cloning to the course

---
+++ Cloning/Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloning
+{
+    class Point : ICloneable
+    {
+        public string Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point() { }
+        public Point(string name, int x, int y) { Name = name; X = x; Y = y; }
+
+        // Override method ToString()
+        public override string ToString() => $"{Name}: ({X},{Y})";
+        public object Clone() => new Point(Name, X, Y);
+
+        // Use this when class does not contain reference types
+        //public object Clone() => this.MemberwiseClone();
+    }
+}
---
+++ Cloning/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Cloning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine
+            (
+                "----------------\n" +
+                "Fun With Cloning\n" +
+                "----------------\n"
+            );
+
+            Point A = new Point("A", 2, 3);
+            Console.WriteLine(A.ToString());
+
+            // Make a clone of Point A
+            Point B = (Point)A.Clone();
+            Console.WriteLine(B.ToString());
+
+            Console.WriteLine
+            (
+                "\n--------------" +
+                "\nEnd of Program" +
+                "\n--------------"
+            );
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
